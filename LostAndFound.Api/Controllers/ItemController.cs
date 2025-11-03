afa1007b89d9a341108b4638fa286d99ddaa209e
Thuthuka Desele <tdesele@insightconsulting.co.za>
2025-11-03 20:52:08 +0200

    Remove auth on items

--- LostAndFound.Api/Controllers/ItemController.cs
+++ LostAndFound.Api/Controllers/ItemController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LostAndFound.Api.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     public class ItemController : ControllerBase
     {
